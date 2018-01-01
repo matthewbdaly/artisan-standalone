a688074759d2c6e46d4c893ccf49f1da14294d82
Matthew Daly <matthewbdaly@gmail.com>
2018-01-01 17:19:14 +0000

    Can now create items

--- src/Base/Application.php
+++ src/Base/Application.php
@@ -4,7 +4,29 @@ namespace Matthewbdaly\ArtisanStandalone\Base;
 
 use Illuminate\Foundation\Application as BaseApplication;
 use Illuminate\Contracts\Foundation\Application as ApplicationContract;
+use RuntimeException;
 
 class Application extends BaseApplication implements ApplicationContract
 {
+    /**
+     * Get the application namespace.
+     *
+     * @return string
+     *
+     * @throws \RuntimeException
+     */
+    public function getNamespace()
+    {
+        if (! is_null($this->namespace)) {
+            return $this->namespace;
+        }
+
+        $composer = json_decode(file_get_contents(base_path('composer.json')), true);
+
+        foreach ((array) data_get($composer, 'autoload.psr-4') as $namespace => $path) {
+            return $this->namespace = $namespace;
+        }
+
+        throw new RuntimeException('Unable to detect application namespace.');
+    }
 }
