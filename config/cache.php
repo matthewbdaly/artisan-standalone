f2745f4c011a41ab2fdb7257226e5be4308da7ef
Matthew Daly <450801+matthewbdaly@users.noreply.github.com>
2019-09-17 20:57:07 +0100

    Fixed an issue with the cache config
    Due to the replacement of the helpers with string objects

--- config/cache.php
+++ config/cache.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Str;
+
 return [
 
     /*
@@ -49,7 +51,7 @@ return [
 
     'prefix' => env(
         'CACHE_PREFIX',
-        str_slug(env('APP_NAME', 'laravel'), '_').'_cache'
+        Str::slug(env('APP_NAME', 'laravel'), '_').'_cache'
     ),
 
 ];
